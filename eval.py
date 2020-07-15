b4c4472e9bdcf9927753ba540974b794d49c81a6
underflow101 <ikarus125@gmail.com>
2020-07-15 18:16:43 +0900

    feat: Add Evaluation source
    - Evaluation Source Code works for linux, mac, windows, and Raspberry Pi
    
    Signed-off-by: underflow101 <ikarus125@gmail.com>

--- eval.py
+++ eval.py
@@ -10,35 +10,31 @@ import cv2
 import numpy as np
 from threading import Thread
 import importlib.util
+from PIL import Image
 
 from cv2 import getTickCount, getTickFrequency
+
 import torch
 import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
 
-def set_input_tensor(interpreter, image):
-    tensor_index = interpreter.get_input_details()[0]['index']
-    input_tensor = interpreter.tensor(tensor_index)()[0]
-    input_tensor[:, :] = image
+from model import SqueezeNet
 
-
-def classify_image(interpreter, image, top_k=1):
-  """Returns a sorted array of classification results."""
-  set_input_tensor(interpreter, image)
-  interpreter.invoke()
-  output_details = interpreter.get_output_details()[0]
-  output = np.squeeze(interpreter.get_tensor(output_details['index']))
-
-  # If the model is quantized (uint8 data), then dequantize the results
-  if output_details['dtype'] == np.uint8:
-    scale, zero_point = output_details['quantization']
-    output = scale * (output - zero_point)
-
-  ordered = np.argpartition(-output, top_k)
-  return [(i, output[i]) for i in ordered[:top_k]]
+from hyperparameter import *
 
 def main():
-    interpreter = tflite.Interpreter(model_path="uint8_model.tflite")
-    interpreter.allocate_tensors()
+    device = torch.device('cpu')
+    model = SqueezeNet(IN_CHANNELS, NUM_CLASSES)
+    weights = torch.load('squeezenet-torch.pth', map_location=torch.device('cpu'))
+    model.load_state_dict(weights)
+    model.eval()
+    
+    config = CONFIG()
+    
+    optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=config.momentum, weight_decay=config.weight_decay)
     
     frame_rate_calc = 1
     freq = getTickFrequency()
@@ -62,53 +58,13 @@ def main():
         sleep = 0
         writing = 0
         for i in range(0, 100):
+            image = Image.open(image_path[j] + str(i) + '.jpg')
+            trans = torchvision.transforms.ToTensor()
+            image = trans(image)
+            data = image.unsqueeze(0)
             t1 = getTickCount()
-            image = cv2.imread(image_path[j] + str(i) + '.jpg')
-            
-            input_details = interpreter.get_input_details()
-            output_details = interpreter.get_output_details()
-
-            # Test model on random input data.
-            input_shape = input_details[0]['shape']
-            input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-            interpreter.set_tensor(input_details[0]['index'], input_data)
-
-            interpreter.invoke()
-            
-            input_details = interpreter.get_input_details()
-            output_details = interpreter.get_output_details()
-            height = input_details[0]['shape'][1]
-            width = input_details[0]['shape'][2]
-
-            floating_model = input_details[0]['dtype'] == np.float32
-            input_mean = 127.5
-            input_std = 127.5
-            min_conf_threshold = 0.2
-            
-            input_data = np.expand_dims(image, axis=0)
-            
-            output_data = interpreter.get_tensor(output_details[0]['index'])
-            results = np.squeeze(output_data)
-
-            if floating_model:
-                input_data = (np.float32(input_data) - input_mean) / input_std
-            
-            top_k = results.argsort()[-4:][::-1]
-            labels = ['writing', 'others', 'phoneWithHand', 'sleep']
-
-            tmp = list()
-            
-            for i in top_k:
-                tmp.append('{:08.6f}: {}'.format(float(results[i]), labels[i]))
-            if(tmp[0].split(" ")[1] == 'writing'):
-                writing += 1
-            elif(tmp[0].split(" ")[1] == 'sleep'):
-                sleep += 1
-            elif(tmp[0].split(" ")[1] == 'phoneWithHand'):
-                phoneWithHand += 1
-            elif(tmp[0].split(" ")[1] == 'others'):
-                others += 1
-            
+            scores = model(data)
+            pred = scores.data.max(1)[1]
             t2 = getTickCount()
             time1 = (t2 - t1) / freq
             frame_rate_calc = 1 / time1
